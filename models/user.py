3515c3600ee24439c6bc7006c681e31625aa8fb3
Maksim Belosokhov <maks3010dan@gmail.com>
2023-12-17 14:19:27 +0300

    Lesson 7 17.12.23, add users, login, signup, logout

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 from flask_login import UserMixin
 
 class User(UserMixin, Base):
-    __tablename__ = "name"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True)
--- routes.py
+++ routes.py
@@ -5,57 +5,65 @@ from .models.group import Group
 from .models.student import Student
 from .models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 
 @app.route("/")
 @app.route("/main")
-@app.route("/group_management", methods=["POST", "GET"])
+@app.route("/group_management")
 def group_manager():
     all_groups = session.query(Group).all()
     all_groups = [x.group_name for x in all_groups]
-
-    if request.method == "POST":
-        group_name = request.form["group_name"]
-        group = Group(
-            group_name=group_name
-        )
-        try:
-            session.add(group)
-            session.commit()
-        except Exception as exc:
-            return f"Виникла проблема: {exc}"
-        finally:
-            session.close()
-        return redirect("/group_management")
     return render_template("group_management.html", group_names=all_groups)
 
+@app.route("/group_management", methods=["POST"])
+@login_required
+def group_management_user():
+    group_name = request.form["group_name"]
+    group = Group(
+        group_name=group_name
+    )
+    try:
+        session.add(group)
+        session.commit()
+    except Exception as exc:
+        return f"Виникла проблема: {exc}"
+    finally:
+        session.close()
+    return redirect("/group_management")
 
-@app.route("/student_management/<group_name>", methods=["POST", "GET"])
-def group_list(group_name):
+
+
+@app.route("/student_management/<group_name>")
+def group_list_as_user(group_name):
     group_id = session.query(Group).where(Group.group_name == group_name).first().id()
-
     group = session.query(Student).where(Student.groups == group_id).all()
-    if request.method == "POST":
-        surname = request.form["surname"]
-        name = request.form["name"]
-        age = request.form["age"]
-        home_address = request.form["home_address"]
-
-        student = Student(
-            surname=surname,
-            name=name,
-            age=age,
-            home_address=home_address
-        )
-        try:
-            session.add(student)
-            session.commit()
-        except Exception as exc:
-            return f"Виникла проблема при збереженні: {exc}"
-        finally:
-            session.close()
-        return redirect(f"student_management/{group_name}")
     return render_template("student_management.html", group=group)
+
+
+@app.route("/student_management/<group_name>", methods=["POST"])
+@login_required
+def group_list_as_user_login(group_name):
+    group_id = session.query(Group).where(Group.group_name == group_name).first().id()
+    surname = request.form["surname"]
+    name = request.form["name"]
+    age = request.form["age"]
+    home_address = request.form["home_address"]
+
+    student = Student(
+        surname=surname,
+        name=name,
+        age=age,
+        home_address=home_address,
+        groups=group_id
+    )
+    try:
+        session.add(student)
+        session.commit()
+    except Exception as exc:
+        return f"Виникла проблема при збереженні: {exc}"
+    finally:
+        session.close()
+    return redirect(f"student_management/{group_name}")
 
 
 @app.route("/signup", methods=["POST", "GET"])
@@ -95,7 +103,7 @@ def login():
             return redirect(url_for("login"))
 
         login_user(user, remember=remember)
-        return redirect(url_for("main"))
+        return redirect("main")
     else:
         return render_template("login.html")
 
@@ -103,3 +111,9 @@ def login():
 @login_manager.user_loader
 def load_user(user_id):
     return session.query(User).get(int(user_id))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("main")
